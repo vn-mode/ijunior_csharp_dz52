618d9126adce5a050150f9fa454f119449df2c60
VITALII NAGIBIN <98809212+vn-mode@users.noreply.github.com>
2023-07-07 20:56:03 +0300

    ijunior_csharp_dz52

---
+++ vn_mode_csharp_dz52/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    private const string BeforeAmnestyMessage = "Список заключенных до амнистии:";
+    private const string AfterAmnestyMessage = "\nСписок заключенных после амнистии:";
+
+    static void Main(string[] args)
+    {
+        List<Prisoner> prisoners = new List<Prisoner>()
+        {
+            new Prisoner("Иванов Иван Иванович", "Антиправительственное"),
+            new Prisoner("Петров Петр Петрович", "Убийство"),
+            new Prisoner("Сидоров Сидор Сидорович", "Антиправительственное"),
+            new Prisoner("Смирнов Александр Александрович", "Грабеж"),
+            new Prisoner("Кузнецов Дмитрий Иванович", "Антиправительственное")
+        };
+
+        Console.WriteLine(BeforeAmnestyMessage);
+
+        foreach (var prisoner in prisoners)
+        {
+            Console.WriteLine($"{prisoner.FullName} - {prisoner.Crime}");
+        }
+
+        prisoners = prisoners.Where(p => p.Crime != "Антиправительственное").ToList();
+
+        Console.WriteLine(AfterAmnestyMessage);
+
+        foreach (var prisoner in prisoners)
+        {
+            Console.WriteLine($"{prisoner.FullName} - {prisoner.Crime}");
+        }
+    }
+}
+
+class Prisoner
+{
+    public string FullName { get; set; }
+    public string Crime { get; set; }
+
+    public Prisoner(string fullName, string crime)
+    {
+        FullName = fullName;
+        Crime = crime;
+    }
+}
